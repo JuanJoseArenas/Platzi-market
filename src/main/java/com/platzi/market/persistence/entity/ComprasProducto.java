c6fa8fd5e245a74a879e4caad4e09b49571a3d4f
Juan Jose Arenas <juanjoseaq99@gmail.com>
2026-02-06 16:55:13 -0500

    Creacion del Dominio Purchase con su respectivo repositorio, servicios y controlador

--- src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
@@ -13,13 +13,13 @@ public class ComprasProducto {
     private boolean estado;
 
     @ManyToOne
+    @MapsId("idCompra")
     @JoinColumn(name = "id_compra", insertable = false,updatable = false)
     private Compra compra;
 
     @ManyToOne
     @JoinColumn(name = "id_producto",insertable = false,updatable = false)
     private Producto producto;
-
 
     public ComprasProductoPK getId() {
         return id;
@@ -51,5 +51,21 @@ public class ComprasProducto {
 
     public void setEstado(boolean estado) {
         this.estado = estado;
+    }
+
+    public Compra getCompra() {
+        return compra;
+    }
+
+    public void setCompra(Compra compra) {
+        this.compra = compra;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
     }
 }
